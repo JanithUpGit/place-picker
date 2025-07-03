8d6ea1a740af8db8cb27db1e292e43a2e3a3a761
P.A.J.U.Pallewela <janithup8@gmail.com>
2025-07-03 12:46:13 +0530

    feat: persist selected places in local storage on selection and removal

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,16 @@ function App() {
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
   useEffect(() => {
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    const storedPlaces = storedIds.map(id => 
+      AVAILABLE_PLACES.find((place) => place.id === id)
+    );
+    setPickedPlaces(storedPlaces);
+  },
+  []
+);
+
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const sortedPlaces = sortPlacesByDistance(
         AVAILABLE_PLACES,
@@ -21,7 +31,7 @@ function App() {
       );
       setAvailablePlaces(sortedPlaces);
     });
-  },[]
+  }, []
   );
 
 
@@ -44,6 +54,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+      );
+    }
+
   }
 
   function handleRemovePlace() {
@@ -51,6 +69,13 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
